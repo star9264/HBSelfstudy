9d8b953c391a813573d3fb5a769e3a117f9b88b1
hb1011 <hb1011@hb1011>
2016-09-13 17:40:54 +0900

    ddd

---
+++ src/baekjoon/Main.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();//¿ù
+		int m = scan.nextInt();//ÀÏ
+		
+		
+		
+		
+	}
+
+}
